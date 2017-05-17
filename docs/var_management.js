ad23e778eb3d8e2fbc5d7414a6df245423c7578a
Papayaman1000 <papayaman1000@gmail.com>
2017-05-17 05:43:04 -0400

    why did I make an infinite while

--- docs/var_management.js
+++ docs/var_management.js
@@ -1,8 +1,6 @@
 var correct = sessionStorage.getItem('correct');
 
-while (true) {
-  if (correct === undefined) {
-    correct = 0;
-    sessionStorage.setItem('correct', 0);
-  }
+while (correct === undefined) {
+  correct = 0;
+  sessionStorage.setItem('correct', 0);
 }
