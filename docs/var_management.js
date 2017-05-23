de078e466e34febef171ae479c4388618aae1f21
Papayaman1000 <papayaman1000@gmail.com>
2017-05-22 21:48:04 -0400

    Update var_management.js

--- docs/var_management.js
+++ docs/var_management.js
@@ -1,6 +1,9 @@
 var correct = sessionStorage.getItem('correct');
 
+var out = document.getElementById("result");
+out = "You got " + correct + " out of 9 questions correct.";
+
 while (correct === undefined) {
-  correct = 0;
   sessionStorage.setItem('correct', 0);
+  correct = sessionStorage.getItem('correct');
 }
