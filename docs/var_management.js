5b85b96f4ded03b20484fd323688ef65c9feec97
Papayaman1000 <papayaman1000@gmail.com>
2017-05-25 17:37:10 -0400

    Working with cookies now.
    Let's hope this works. In the words of Papa DevOps: "Everybody has a testing environment. Some people are lucky enough enough to have a totally separate environment to run production in." I am not _remotely_ lucky enough.

--- docs/var_management.js
+++ docs/var_management.js
@@ -1,9 +1,31 @@
-var correct = sessionStorage.getItem('correct');
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
-var out = document.getElementById("result");
-out = "You got " + correct + " out of 9 questions correct.";
+var score = getCookie("score");
 
-while (correct === undefined) {
-  sessionStorage.setItem('correct', 0);
-  correct = sessionStorage.getItem('correct');
+if (!score) {
+    document.cookie = "score=0; expires=Fri, 18 Dec 2048 12:00:00 UTC; path=/";
+    score = getCookie("score");
+}
+
+function logCookie() {
+    console.alert("The cookie is currently: " + score);
+}
+
+function updateCookie() {
+    document.cookie = "score=" + score + "; expires=Fri, 18 Dec 2048 12:00:00 UTC; path=/";
+    logCookie();
 }
